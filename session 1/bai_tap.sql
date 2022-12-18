fe3dcf6b55b3dc3a5bf1dd307fa8627cadb76f3c
Phanhatuan <louisoberit2108@gmail.com>
2022-12-18 21:57:35 +0700

    add

---
+++ session 1/bai_tap.sql
@@ -0,0 +1,12 @@
+create database session1;
+use session1;
+create table Class(
+	id int,
+    name varchar(255)
+);
+ create table Teacher(
+	id int,
+    name varchar(255),
+    age int,
+    country varchar(255)
+ );
