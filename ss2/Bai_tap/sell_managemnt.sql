942de55f542fa49e2cb7b85b196bbb8d6f76f634
Phanhatuan <louisoberit2108@gmail.com>
2022-12-19 20:32:43 +0700

    add

--- ss2/Bai_tap/sell_managemnt.sql
+++ ss2/Bai_tap/sell_managemnt.sql
@@ -1,3 +1,28 @@
 create database sell_management;
 use sell_management;
-create
+create table customer (
+	cId int primary key,
+    cName varchar(255),
+    cAge int
+);
+create table orders(
+	oId int primary key,
+    cId int,
+    oDate DATE,
+    oTotalPrice int,
+    CONSTRAINT fk_cId FOREIGN KEY(cId) REFERENCES customer(cId)
+);
+create table product(
+	pId int primary key,
+    pName varchar(255),
+    pPrice  varchar(255)
+);
+drop table orders_detail;
+create table orders_detail(
+	oId int ,
+    pId int ,
+    primary key(oId,pId),
+    odQTY int,
+    CONSTRAINT fk_oId FOREIGN key (oId) REFERENCES orders(oId),
+	CONSTRAINT fk_pId FOREIGN key (pId) REFERENCES product(pId)
+);
